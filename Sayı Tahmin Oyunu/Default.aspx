9f8499997bf6b301ddde3354136952c4acd2b1e8
aykutgursel <aykutgursel9@gmail.com>
2018-05-29 16:37:45 +0300

    Sayı tahmin oyunu

---
+++ Sayı Tahmin Oyunu/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblHak" runat="server" Text="Kalan Hak : "></asp:Label>
+        <br />
+        <br />
+        <asp:TextBox ID="TxSayi" runat="server"></asp:TextBox>
+        <asp:Button ID="btnTahmin" runat="server" Text="Tahmin" OnClick="btnTahmin_Click"  />
+        <asp:Button ID="btnRestart" runat="server" Text="Yeniden Başlat" Enabled="false" OnClick="btnRestart_Click" />
+        <br />
+        <br />
+        <asp:Label ID="lblDurum" runat="server" Text="Label"></asp:Label>
+    
+        <br />
+        <asp:Label ID="SonTahmin" runat="server" Text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Sayı Tahmin Oyunu/Default.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    Random Rsayi = new Random();
+
+    private int RasgeleSayi
+    {
+        get
+        {
+            if (ViewState["RasgeleSayi"] != null)
+            {
+                return ((int)ViewState["RasgeleSayi"]);
+            }
+            else
+            {
+                ViewState["RasgeleSayi"] = 0;
+                return 0;
+            }
+        }
+        set
+        {
+            ViewState["RasgeleSayi"] = value;
+        }
+    }
+
+    private int KalanHak
+    {
+        get
+        {
+            if (ViewState["KalanHak"] != null)
+            {
+                return ((int)ViewState["KalanHak"]);
+            }
+            else
+            {
+                ViewState["KalanHak"] = 0;
+                return 0;
+            }
+        }
+        set
+        {
+            ViewState["KalanHak"] = value;
+        }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!Page.IsPostBack)
+        {
+            RasgeleSayi = Rsayi.Next(0, 50);
+            KalanHak = 5;
+            lblHak.Text = "Kalan Hakkınız : " + KalanHak.ToString();
+            lblDurum.Text = "";
+        }
+
+    }
+    protected void btnTahmin_Click(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(TxSayi.Text))
+        {
+
+            if (Convert.ToInt32(TxSayi.Text) < RasgeleSayi)
+            {
+                lblDurum.Text = "Sonuç biraz daha büyük";
+                KalanHak--;
+            }
+
+            if (Convert.ToInt32(TxSayi.Text) > RasgeleSayi)
+            {
+                lblDurum.Text = "Sonuç biraz küçük";
+                KalanHak--;
+            }
+
+            if (Convert.ToInt32(TxSayi.Text) == RasgeleSayi)
+            {
+                lblDurum.Text = "TEBRIKLER! Cevabı "+KalanHak.ToString()+" hakkın kala buldun";
+                btnRestart.Enabled = true;
+                btnTahmin.Enabled = false;
+            }
+
+            if (KalanHak == 0)
+            {
+                lblDurum.Text = "Hakkınız Kalmadı. Sonuç : "+RasgeleSayi;
+                btnRestart.Enabled = true;
+                btnTahmin.Enabled = false;
+            }
+
+            lblHak.Text = "Kalan Hakkınız : " + KalanHak.ToString();
+            SonTahmin.Text = "Son Tahmininiz : " + TxSayi.Text;
+            TxSayi.Text = string.Empty;
+
+        }
+
+    }
+    protected void btnRestart_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Default.aspx");
+    }
+
+}
